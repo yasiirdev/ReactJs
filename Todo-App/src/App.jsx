7336a6316f4dc7a9a6d4333596702384ce1abe97
yasiirdev <yasiirdev@gmail.com>
2025-11-11 18:23:23 +0500

    modal send two value to app.Jsx

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -6,24 +6,12 @@ import Header from "./Components/Header";
 
 function App() {
   let [userTask, setuserTask] = useState([]);
-  let [heading, setheading] = useState("");
-  let [task, settask] = useState("");
   let [taskCount, settaskCount] = useState(0);
 
-  // this function set the first input heading value with synthetic event
-  let HeadingHandle = (e) => {
-    e.preventDefault();
-    setheading(e.target.value);
-  };
-
-  // this function set the second textarea description value with synthetic event
-  let TaskHandle = (e) => {
-    e.preventDefault();
-    settask(e.target.value);
-  };
+  
 
   // save the task when user press the save button on modal box
-  let saveHandle = function () {
+  let saveHandle = function (heading , task) {
     // if the value is not trim() function return
     if (!heading.trim() || !task.trim()) {
       return;
@@ -38,8 +26,6 @@ function App() {
 
     setuserTask([...userTask, newTask]);
     settaskCount(taskCount + 1);
-    setheading("");
-    settask("");
   };
 
   // all list contain a delete button with a uniqe num called taskCount
@@ -57,11 +43,7 @@ function App() {
         <Header taskCount={taskCount} />
         <ShowTask userTask={userTask} onDelete={deleteHandle} />
         <Modal
-          HeadingHandle={HeadingHandle}
-          TaskHandle={TaskHandle}
           saveHandle={saveHandle}
-          heading={heading}
-          task={task}
         />
       </Container>
     </>
