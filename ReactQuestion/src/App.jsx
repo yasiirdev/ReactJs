c28bd76390e4bb3da1c12920ad9212126dd16b07
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-11-12 18:59:33 +0500

    toggle button that play or pause the video

--- ReactQuestion/src/App.jsx
+++ ReactQuestion/src/App.jsx
@@ -1,11 +1,47 @@
-import Navbar from "./components/navbar"
-function App() {
+import { useState, useRef } from "react";
+
+export default function VideoPlayer() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const playingControl = useRef(null);
+
+  function handleClick() {
+    const nextIsPlaying = !isPlaying;
+    nextIsPlaying === true ? playingControl.current.play() : playingControl.current.pause();
+    setIsPlaying(nextIsPlaying);
+
+  }
 
   return (
     <>
-    <Navbar />
+      <button onClick={handleClick}>{isPlaying ? "Pause" : "Play"}</button>
+      <video width="250" ref={playingControl}>
+        <source
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+          type="video/mp4"
+        />
+      </video>
     </>
-  )
+  );
 }
 
-export default App
+// import { useRef } from "react";
+
+// export default function Page() {
+
+//   const inputRef = useRef(null);
+  
+//   const HandleFocus = () => {
+//     inputRef.current.focus();
+//     console.log("done");
+    
+//   };
+
+//   return (
+//     <>
+//       <nav>
+//         <button onClick={HandleFocus}>Search</button>
+//       </nav>
+//       <input ref={inputRef} placeholder="Looking for something?" />
+//     </>
+//   );
+// }
