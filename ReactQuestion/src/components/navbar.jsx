a99d411f807924772a543bee1d841fb9498552e7
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-11-12 19:00:54 +0500

    new navbar component

--- ReactQuestion/src/components/navbar.jsx
+++ ReactQuestion/src/components/navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
   return (
     <>
-      <div classNameName="navbar w-full h-[6%]  bg-amber-200 fixed top-0 flex items-center justify-items-center place-content-around ">
+      <nav classNameName="navbar w-full h-[6%]  bg-amber-200 fixed top-0 flex items-center justify-items-center place-content-around ">
         <a href="#">Contact</a>
         <a href="#">Setting</a>
         <a href="#">Profile</a>
@@ -13,7 +13,7 @@ export default function Navbar() {
           placeholder="Search Here ..."
           classNameName="bar placeholder: text-black border-1 rounded-2xl w-[30%] h-[30px]  font-mono"
         />
-      </div>
+      </nav>
     </>
   );
 }
