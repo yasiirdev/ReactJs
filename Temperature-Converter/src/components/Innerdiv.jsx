f8f79edf5020b75447a4ae6384756f36dbc02f7f
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-11-20 18:35:30 +0500

    some update or errors

--- Temperature-Converter/src/components/Innerdiv.jsx
+++ Temperature-Converter/src/components/Innerdiv.jsx
@@ -1,18 +1,36 @@
 export function Innerdiv() {
+ function showMenu() {
+  let option = document.getElementById("myoption");
+   option.style.display === "none"
+     ? (option.style.display = "grid")
+     : (option.style.display = "none");
+ }
+
+ const menu = document.getElementById("mymenu");
+
+ menu.addEventListener("mouseenter", showMenu);
+
+ menu.addEventListener("mouseleave", () => {
+  let option2 = option.style.display = "none";
+ });
+ 
+ 
   return (
     <>
       <div className="innerdiv">
         <input type="number" placeholder="0.00" className="inputNum" required />
-        <select name="fromUnit" id="fromUnit">
-          <option value="">celseus</option>
-          <option value="">fehrenheit</option>
-          <option value="">kelvin</option>
-        </select>
-        <select name="toUnit" id="toUnit">
-          <option value="">celseus</option>
-          <option value="">fehrenheit</option>
-          <option value="">kelvin</option>
-        </select>
+        <div name="Many" class="menu" id="mymenu">
+          <p>Links</p>
+          <div class="option" id="myoption">
+      
+          </div>
+        </div>
+        <div name="Many" class="menu" id="mymenu">
+          <p>Links</p>
+          <div class="option" id="myoption">
+
+          </div>
+        </div>
       </div>
     </>
   );
