be8dfa84dda5fe4a3e5b0615618741d558cdfc38
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-11-22 19:00:22 +0500

    Core logic

--- Temperature-Converter/src/App.jsx
+++ Temperature-Converter/src/App.jsx
@@ -1,11 +1,44 @@
-import { Outerdiv } from "./components/Outerdiv";
-export default function Converter() {
-  
+import { HtmlForConvertor } from "./components/HtmlForConvertor";
+export default function temperatureConverter() {
+    // round of to a specific number
+  function roundToDecimalPlaces(number, decimalPlaces) {
+    const multiplier = Math.pow(10, decimalPlaces);
+    return Math.round(number * multiplier) / multiplier;
+  }
+
+  const unitsHandle = (val, fromUnit, toUnit , pRef ) => {
+    try {
+      if (fromUnit.value === "Celsius" && toUnit.value === "fehranheit") {
+        let f = (val * 9) / 5 + 32;
+        pRef.current.innerText = `${val} Celsius is ${f} Fehranheit`;
+      } else if (fromUnit.value === "Celsius" && toUnit.value === "kelvin") {
+        let k = roundToDecimalPlaces(Number(val) + 273.15, 3);
+        pRef.current.innerText = `${val} Celsius is ${k} kelvin`;
+      } else if (
+        fromUnit.value === "fehranheit" &&
+        toUnit.value === "Celsius"
+      ) {
+        let c = roundToDecimalPlaces((val - 32) / 1.8, 3);
+         pRef.current.innerText = `${val} Fehranheit is ${c} Calsius`;
+      } else if (fromUnit.value === "fehranheit" && toUnit.value === "kelvin") {
+        let ftok = roundToDecimalPlaces(((val - 32) * 5) / 9 + 273.15, 3);
+         pRef.current.innerText = `${val} Fehranheit is ${ftok} Kelvin`;
+      } else if (fromUnit.value === "kelvin" && toUnit.value === "Celsius") {
+        let ktoc = roundToDecimalPlaces(val - 273.15, 3);
+         pRef.current.innerText = `${val} Kelvin is ${ktoc} Calsius`;
+      } else if (fromUnit.value === "kelvin" && toUnit.value === "fehranheit") {
+        let ktof = roundToDecimalPlaces(((val - 273.15) * 9) / 5 + 32, 3);
+         pRef.current.innerText = `${val} Kelvin is ${ktof} Fehranheit`;
+      }
+    } catch {
+      console.log(`Error in formulaz`);
+    }
+    // fehranheit  kelvin
+  };
 
   return (
     <>
-      <Outerdiv></Outerdiv>
+      <HtmlForConvertor unitsHandle={unitsHandle}  />
     </>
   );
-
 }
