31635999cbee2c2fe429638011f58a4c674950d7
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-11-25 18:29:33 +0500

    Core logic with simple use method

--- Temperature-Converter/src/App.jsx
+++ Temperature-Converter/src/App.jsx
@@ -1,35 +1,59 @@
 import { HtmlForConvertor } from "./components/HtmlForConvertor";
+
 export default function temperatureConverter() {
-    // round of to a specific number
+  // round of to a specific number
   function roundToDecimalPlaces(number, decimalPlaces) {
     const multiplier = Math.pow(10, decimalPlaces);
     return Math.round(number * multiplier) / multiplier;
   }
 
-  const unitsHandle = (val, fromUnit, toUnit , pRef ) => {
+  const unitsHandle = (val, fromUnit, toUnit, pRef) => {
     try {
+      // Celsius --> fehranheit
       if (fromUnit.value === "Celsius" && toUnit.value === "fehranheit") {
         let f = (val * 9) / 5 + 32;
         pRef.current.innerText = `${val} Celsius is ${f} Fehranheit`;
-      } else if (fromUnit.value === "Celsius" && toUnit.value === "kelvin") {
+      }
+      // Celsius --> Kelvin
+      else if (fromUnit.value === "Celsius" && toUnit.value === "kelvin") {
         let k = roundToDecimalPlaces(Number(val) + 273.15, 3);
         pRef.current.innerText = `${val} Celsius is ${k} kelvin`;
-      } else if (
-        fromUnit.value === "fehranheit" &&
-        toUnit.value === "Celsius"
-      ) {
-        let c = roundToDecimalPlaces((val - 32) / 1.8, 3);
-         pRef.current.innerText = `${val} Fehranheit is ${c} Calsius`;
-      } else if (fromUnit.value === "fehranheit" && toUnit.value === "kelvin") {
-        let ftok = roundToDecimalPlaces(((val - 32) * 5) / 9 + 273.15, 3);
-         pRef.current.innerText = `${val} Fehranheit is ${ftok} Kelvin`;
-      } else if (fromUnit.value === "kelvin" && toUnit.value === "Celsius") {
-        let ktoc = roundToDecimalPlaces(val - 273.15, 3);
-         pRef.current.innerText = `${val} Kelvin is ${ktoc} Calsius`;
-      } else if (fromUnit.value === "kelvin" && toUnit.value === "fehranheit") {
-        let ktof = roundToDecimalPlaces(((val - 273.15) * 9) / 5 + 32, 3);
-         pRef.current.innerText = `${val} Kelvin is ${ktof} Fehranheit`;
       }
+      // Celsius --> celsius
+      else if (fromUnit.value === "Celsius" && toUnit.value === "Celsius") {
+        pRef.current.innerText = `${val} Celsius is ${val} Celsius`;
+      }
+      //  fehranheit --> celsius
+      else if (fromUnit.value === "fehranheit" && toUnit.value === "Celsius") {
+        let c = roundToDecimalPlaces((val - 32) / 1.8, 3);
+        pRef.current.innerText = `${val} Fehranheit is ${c} Celsius`;
+      }
+      // fehranheit --> kelvin
+      else if (fromUnit.value === "fehranheit" && toUnit.value === "kelvin") {
+        let ftok = roundToDecimalPlaces(((val - 32) * 5) / 9 + 273.15, 3);
+        pRef.current.innerText = `${val} Fehranheit is ${ftok} Kelvin`;
+      }
+      // fehranheit --> fehranheit
+      else if (
+        fromUnit.value === "fehranheit" &&
+        toUnit.value === "fehranheit"
+      ) {
+        pRef.current.innerText = `${val} Fehranheit is ${val} fehranheit`;
+      }
+      // kelvin --> Celsius
+      else if (fromUnit.value === "kelvin" && toUnit.value === "Celsius") {
+        let ktoc = roundToDecimalPlaces(val - 273.15, 3);
+        pRef.current.innerText = `${val} Kelvin is ${ktoc} Calsius`;
+      }
+      // kelvin --> fehranheit
+      else if (fromUnit.value === "kelvin" && toUnit.value === "fehranheit") {
+        let ktof = roundToDecimalPlaces(((val - 273.15) * 9) / 5 + 32, 3);
+        pRef.current.innerText = `${val} Kelvin is ${ktof} Fehranheit`;
+      }
+      // kelvin --> kelvin
+      else if (fromUnit.value === "kelvin" && toUnit.value === "kelvin") {
+        pRef.current.innerText = `${val} Kelvin is ${val} Kelvin`;
+      }      
     } catch {
       console.log(`Error in formulaz`);
     }
@@ -38,7 +62,7 @@ export default function temperatureConverter() {
 
   return (
     <>
-      <HtmlForConvertor unitsHandle={unitsHandle}  />
+      <HtmlForConvertor unitsHandle={unitsHandle} />
     </>
   );
 }
