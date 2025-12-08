177f1134e9fa32b520ca59bef1a7cad04e38602e
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-08 18:28:53 +0500

    Footer component

--- Social-Media/src/Components/Footer.jsx
+++ Social-Media/src/Components/Footer.jsx
@@ -1,39 +1,44 @@
-export function Footer() {
+export default function Footer() {
     return (
-      <div class="container">
-        {" "}
-        <footer class="py-3 my-4">
-          {" "}
-          <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <footer className="footer mt-auto py-3 bg-light">
+        <div className="container">
             {" "}
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Home
-              </a>
-            </li>{" "}
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Features
-              </a>
-            </li>{" "}
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                Pricing
-              </a>
-            </li>{" "}
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                FAQs
-              </a>
-            </li>{" "}
-            <li class="nav-item">
-              <a href="#" class="nav-link px-2 text-body-secondary">
-                About
-              </a>
-            </li>{" "}
-          </ul>{" "}
-          <p class="text-center text-body-secondary">© 2025 Company, Inc</p>{" "}
-        </footer>{" "}
-      </div>
+            <footer className="py-3 my-4">
+              {" "}
+              <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                {" "}
+                <li className="nav-item">
+                  <a href="#" className="nav-link px-2 text-body-secondary">
+                    Home
+                  </a>
+                </li>
+                <li name="nav-item">
+                  <a href="#" className="nav-link px-2 text-body-secondary">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link px-2 text-body-secondary">
+                    Pricing
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link px-2 text-body-secondary">
+                    FAQs
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a href="#" className="nav-link px-2 text-body-secondary">
+                    About
+                  </a>
+                </li>
+              </ul>
+              <p className="text-center text-body-secondary">
+                © 2025 Company, Inc
+              </p>
+            </footer>
+          </div>
+      </footer>
     );
+      
 }
