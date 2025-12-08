d7d750d8cbe34f1214d153ef527fe716321adf19
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-08 18:33:19 +0500

    bootstrap

--- Social-Media/src/main.jsx
+++ Social-Media/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+//import { createBrowserRouter, RouterProvider } from "react-router";
 import './index.css'
 import App from './App.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// const router = createBrowserRouter([{
+//  path:"/" , element: <App />
+// },
+// ]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <RouterProvider router={router} /> */}
     <App />
   </StrictMode>,
 )
