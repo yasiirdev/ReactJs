5accd469eb5b6ee22eafa7171fe0c2e1d3437643
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-08 18:30:23 +0500

    Hero Part of this website to show post

---
+++ Social-Media/src/Components/Hero.jsx
@@ -0,0 +1,33 @@
+import Card from "./Card";
+
+export default function Hero() {
+  return (
+    <div className="content">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
