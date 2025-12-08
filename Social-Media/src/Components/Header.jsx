9d400a45459ea3897ba29929cf7018ebb7810fd1
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-08 18:29:21 +0500

    Header component

--- Social-Media/src/Components/Header.jsx
+++ Social-Media/src/Components/Header.jsx
@@ -1,74 +1,83 @@
-export function Header() {
+import { useRef } from "react";
+
+export default function Header({ isCLicked, NonClicked }) {
+  const anchorHome = useRef(null);
+  const anchorCreatePost = useRef(null);
+
+  function openPost() {
+    anchorCreatePost.current.className = "nav-link px-2 text-secondary";
+    anchorHome.current.className = "nav-link px-2 text-white";
+    isCLicked();
+  }
+
+  function openHome() {
+    anchorHome.current.className = "nav-link px-2 text-secondary";
+    anchorCreatePost.current.className = "nav-link px-2 text-white";
+    
+    NonClicked();
+  }
+
   return (
-    <header class="p-3 text-bg-dark">
-      {" "}
-      <div class="container">
-        {" "}
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          {" "}
+    <header className="p-3 text-bg-dark">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
           <a
             href="/"
-            class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
           >
-            {" "}
             <svg
-              class="bi me-2"
+              className="bi me-2"
               width="40"
               height="32"
               role="img"
               aria-label="Bootstrap"
             >
-              <use xlink:href="#bootstrap"></use>
-            </svg>{" "}
-          </a>{" "}
-          <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            {" "}
+              <use xlinkHref="#bootstrap"></use>
+            </svg>
+          </a>
+          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             <li>
-              <a href="#" class="nav-link px-2 text-secondary">
+              <a
+                href="#"
+                className="nav-link px-2 text-secondary"
+                onClick={openHome}
+                ref={anchorHome}
+              >
                 Home
               </a>
-            </li>{" "}
+            </li>
             <li>
-              <a href="#" class="nav-link px-2 text-white">
-                Features
+              <a
+                href="#"
+                className="nav-link px-2 text-white"
+                onClick={openPost}
+                ref={anchorCreatePost}
+              >
+                Create Post
               </a>
-            </li>{" "}
-            <li>
-              <a href="#" class="nav-link px-2 text-white">
-                Pricing
-              </a>
-            </li>{" "}
-            <li>
-              <a href="#" class="nav-link px-2 text-white">
-                FAQs
-              </a>
-            </li>{" "}
-            <li>
-              <a href="#" class="nav-link px-2 text-white">
-                About
-              </a>
-            </li>{" "}
-          </ul>{" "}
-          <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-            {" "}
+            </li>
+          </ul>
+          <form
+            className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
+            role="search"
+          >
             <input
               type="search"
-              class="form-control form-control-dark text-bg-dark"
+              className="form-control form-control-dark text-bg-dark searchSize"
               placeholder="Search..."
               aria-label="Search"
-            />{" "}
-          </form>{" "}
-          <div class="text-end">
-            {" "}
-            <button type="button" class="btn btn-outline-light me-2">
+            />
+          </form>
+          {/* <div className="text-end">
+            <button type="button" className="btn btn-outline-light me-2">
               Login
-            </button>{" "}
-            <button type="button" class="btn btn-warning">
+            </button>
+            <button type="button" className="btn btn-warning">
               Sign-up
-            </button>{" "}
-          </div>{" "}
-        </div>{" "}
-      </div>{" "}
+            </button>
+          </div> */}
+        </div>
+      </div>
     </header>
   );
 }
