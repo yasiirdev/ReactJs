233c065467fdbccf697c62ecdafe564e2211ad4d
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-23 17:40:16 +0500

    the main page where all data is posted

--- Social-Media/src/Components/Hero.jsx
+++ Social-Media/src/Components/Hero.jsx
@@ -1,13 +1,23 @@
 import { useContext } from "react";
 import PostContext from "../Context/ContextApi";
-import Card from "./Card";
+import Post from "./post";
+
 export default function Hero() {
-  const { postState } = useContext(PostContext);
+  const { postState, addPosts } = useContext(PostContext);
+
+  function HandleBt() {
+    fetch("https://dummyjson.com/posts", {
+      method: "POST"
+    }).then((res) => res.json()).then((obj) => addPosts(obj.posts)).catch(error => console.log(error));
+  }
+
+
   return (
     <div className="content">
       {postState.map((postlist) => (
-        <Card key={postlist.id} postlist={postlist} />
+        <Post key={postlist.id} postlist={postlist} />
       ))}
+      <button type="button" onClick={HandleBt}>Fetch</button>
     </div>
   );
 }
