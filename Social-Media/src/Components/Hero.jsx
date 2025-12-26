3ac6037a6ad0969fac5392a1ffba690766c7210d
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-26 17:28:19 +0500

    the main page where all data is posted with leader

--- Social-Media/src/Components/Hero.jsx
+++ Social-Media/src/Components/Hero.jsx
@@ -1,23 +1,38 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import PostContext from "../Context/ContextApi";
 import Post from "./post";
+import Leading from "./Spinner";
 
 export default function Hero() {
   const { postState, addPosts } = useContext(PostContext);
+  const [fetched, setFetched] = useState(false);
 
-  function HandleBt() {
+  useEffect(() => {
+     setFetched(true);
     fetch("https://dummyjson.com/posts", {
-      method: "POST"
-    }).then((res) => res.json()).then((obj) => addPosts(obj.posts)).catch(error => console.log(error));
-  }
-
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((obj) => {
+        addPosts(obj.posts);
+        setFetched(false);
+      })
+      .catch((error) => console.log(error));
+  }, [addPosts]); 
 
   return (
-    <div className="content">
-      {postState.map((postlist) => (
-        <Post key={postlist.id} postlist={postlist} />
-      ))}
-      <button type="button" onClick={HandleBt}>Fetch</button>
-    </div>
+    <>
+      {fetched && (
+        <div className="leading">
+          <Leading />
+        </div>
+      )}
+      <div className="content">
+        {!fetched &&
+          postState.map((postlist) => (
+            <Post key={postlist.id} postlist={postlist} />
+          ))}
+      </div>
+    </>
   );
 }
