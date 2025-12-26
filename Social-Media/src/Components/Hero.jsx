5937a3e1c37a394213be6720c88ed07b70eb7df4
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-26 17:39:00 +0500

    using a spinner to asyro process

--- Social-Media/src/Components/Hero.jsx
+++ Social-Media/src/Components/Hero.jsx
@@ -5,30 +5,32 @@ import Leading from "./Spinner";
 
 export default function Hero() {
   const { postState, addPosts } = useContext(PostContext);
-  const [fetched, setFetched] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-     setFetched(true);
     fetch("https://dummyjson.com/posts", {
       method: "GET",
     })
       .then((res) => res.json())
       .then((obj) => {
         addPosts(obj.posts);
-        setFetched(false);
+        setLoading(false);
       })
-      .catch((error) => console.log(error));
-  }, [addPosts]); 
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [addPosts]);
 
   return (
     <>
-      {fetched && (
+      {loading && (
         <div className="leading">
           <Leading />
         </div>
       )}
       <div className="content">
-        {!fetched &&
+        {!loading &&
           postState.map((postlist) => (
             <Post key={postlist.id} postlist={postlist} />
           ))}
