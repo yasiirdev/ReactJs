121f20ddc32903890891f9c4c3fe27a3896d7193
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2025-12-26 17:30:22 +0500

    leading spinner

---
+++ Social-Media/src/Components/Spinner.jsx
@@ -0,0 +1,16 @@
+const Leading = () => {
+    return (
+      <button className="btn btn-primary" type="button" disabled>
+        <span
+          className="spinner-border spinner-border-sm"
+          role="status"
+          aria-hidden="true"
+        ></span>
+        Loading...
+      </button>
+    );
+}
+
+
+
+export default Leading;
