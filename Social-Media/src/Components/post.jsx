6541fcf64b43301e81a1d625c3dcdcf7409a6d3f
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-02 17:50:10 +0500

    post contain title , body and username

--- Social-Media/src/Components/post.jsx
+++ Social-Media/src/Components/post.jsx
@@ -7,18 +7,12 @@ const Post = ({ postlist }) => {
   const deletefun = () => {
     deletepost(postlist.id);
   };
+  
   return (
     <div className="card" id="design_card" style={{ width: "18rem" }}>
       <div className="card-body">
         <h5 className="card-title">{postlist.title}</h5>
         <p className="card-text">{postlist.body}</p>
-      </div>
-      <div className="postTags">
-        {postlist.tags.map((tag) => (
-          <span key={tag} className="badge text-bg-info">
-            {tag}
-          </span>
-        ))}
       </div>
       <span
         className="position-absolute top-0 start-100 translate-middle "
@@ -26,9 +20,6 @@ const Post = ({ postlist }) => {
       >
         <MdDelete />
       </span>
-      <div className="alert alert-primary" role="alert" key={postlist.userId}>
-        {`👍🏼 ${postlist.reactions.likes} likes   👎${postlist.reactions.dislikes} dislikes `}
-      </div>
     </div>
   );
 };
