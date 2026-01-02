3614042655b7fe8fec07f63149a2e597d6544c33
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-02 17:49:01 +0500

    now the data saftly move to jsonplaceholder fake server

--- Social-Media/src/Components/New.jsx
+++ Social-Media/src/Components/New.jsx
@@ -1,34 +1,54 @@
-import { useRef } from "react";
-
+import { useContext,  useRef } from "react";
+import { useOnline } from "../Hooks/networkStatus";
+import PostContext from "../Context/ContextApi";
 export default function CreatePost() {
-  const input_1 = useRef(null);
-  const textarea_1 = useRef(null);
-  const input_2 = useRef(null);
-  const tags = useRef(null);
-  const user_id = useRef(null);
+  const userId = useRef(null);
+  const body = useRef(null);
+  const title = useRef(null);
+
+  // custom hook
+  const isonline = useOnline();
+  const { pushPost } = useContext(PostContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    let title = input_1.current.value;
-    let description = textarea_1.current.value;
-    let reaction = input_2.current.value;
-    let tagsInarr = tags.current.value.split(/(\s+)/);
-    let user_name = user_id.current.value;
+    let I = userId.current.value;
+    let B = body.current.value;
+    let T = title.current.value;
 
-    console.log(title, description, reaction, tagsInarr , user_name);
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: T.trim(),
+        body: B.trim(),
+        userId: I.trim(),
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          throw new Error("Method : POST is fail  due to unstable network");
+        }
+      })
+      .then((json) => {
+        console.log(json);
+        return pushPost(json);
+      }).catch(error => console.log(error));
 
-     
-    input_1.current.value = "";
-    textarea_1.current.value = "";
-    input_2.current.value = "";
-    tags.current.value = "";
-    user_id.current.value = "";
+    userId.current.value = "";
+    body.current.value = "";
+    title.current.value = "";
+
     return 0;
   };
 
   return (
     <form className="formControler" id="newposts" name="createnewpost">
-      <div className="mb-3 createPostForm">
+      <div className="mb-3 createPostForm" id="postfield">
         <label
           htmlFor="Input1"
           className="form-label"
@@ -39,9 +59,9 @@ export default function CreatePost() {
         <input
           type="text"
           className="form-control"
-          id="Input1"
+          id="userId"
           placeholder="Enter username"
-          ref={user_id}
+          ref={userId}
         />
       </div>
 
@@ -56,9 +76,9 @@ export default function CreatePost() {
         <input
           type="text"
           className="form-control"
-          id="Input1"
+          id="title"
           placeholder="Title"
-          ref={input_1}
+          ref={title}
         />
       </div>
       <div className="mb-3">
@@ -67,42 +87,20 @@ export default function CreatePost() {
         </label>
         <textarea
           className="form-control"
-          id="Textarea1"
+          id="body"
           rows="3"
-          ref={textarea_1}
+          ref={body}
         ></textarea>
       </div>
-
-      <div className="mb-3">
-        <label htmlFor="input2" className="form-label" title="🥰🥶😱❤️">
-          Reaction
-        </label>
-        <input
-          type="number"
-          name="input2"
-          id="input2"
-          className="customForm"
-          placeholder="reaction..."
-          ref={input_2}
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="input2" className="form-label" title="🥰🥶😱❤️">
-          Tags
-        </label>
-        <input
-          type="text"
-          name="tags"
-          id="tags"
-          className="form-control"
-          ref={tags}
-          placeholder="Enter tags with space"
-        />
-      </div>
-
-      <button type="submit" className="custom-button" onClick={handleSubmit}>
-        Post
-      </button>
+      {isonline ? (
+        <button type="submit" className="custom-button" onClick={handleSubmit}>
+          Post
+        </button>
+      ) : (
+        <button type="submit" disabled className="disabled-button">
+          Reconnecting...
+        </button>
+      )}
     </form>
   );
 }
