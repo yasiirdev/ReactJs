5d143dd7a95ea12d5236fe6d828bb8e09a831438
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-04 15:45:44 +0500

    context and context provider

--- Social-Media/src/Context/ContextApi.jsx
+++ Social-Media/src/Context/ContextApi.jsx
@@ -1,3 +1,4 @@
 import { createContext } from "react";
- const PostContext = createContext(null);
+const PostContext = createContext([]);
+ 
 export default PostContext;
--- Social-Media/src/Context/ContextProvider.jsx
+++ Social-Media/src/Context/ContextProvider.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useReducer } from "react";
+import { useCallback, useReducer, useEffect, useState } from "react";
 import PostContext from "./ContextApi";
 
 const reducerFun = (preValue, action) => {
@@ -8,16 +8,19 @@ const reducerFun = (preValue, action) => {
   } else if (action.type === "DELETE_POST") {
     new_post = action.payload.newPost;
   } else if (action.type === "USER_POST") {
-    console.log(action.payload.user_post);
-    new_post = new_post.unshift(action.payload.user_post);
+    // console.log(action.payload.user_post);
+    new_post = [action.payload.user_post, ...preValue];
   }
   return new_post;
 };
 
 export const PostContextProvider = ({ children }) => {
   let [postState, disptachPost] = useReducer(reducerFun, []);
+  const [loading, setLoading] = useState(true);
 
-  const deletepost =(post_id) => {
+  
+  const deletepost = (post_id) => {
+    
     let newPost = postState.filter((post) => {
       return post.id !== post_id;
     });
@@ -26,8 +29,8 @@ export const PostContextProvider = ({ children }) => {
       type: "DELETE_POST",
       id: "deletePost",
       payload: {
-        newPost
-      }
+        newPost,
+      },
     });
   };
 
@@ -36,24 +39,51 @@ export const PostContextProvider = ({ children }) => {
       id: "userpost",
       type: "USER_POST",
       payload: {
-        user_post
-      }
-    })
-
+        user_post,
+      },
+    });
   };
 
-  const addPosts = useCallback((posts) => {
-    disptachPost({
-      type: "ADD_POST",
-      id: "addPost",
-      payload: {
-        posts
-      }
-    });
-  },[disptachPost]);
+  const addPosts = useCallback(
+    (posts) => {
+      disptachPost({
+        type: "ADD_POST",
+        id: "addPost",
+        payload: {
+          posts,
+        },
+      });
+    },
+    [disptachPost]
+  );
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+
+    fetch("https://jsonplaceholder.typicode.com/posts", { signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("error dua to unstable network");
+        } else {
+          return res.json();
+        }
+      })
+      .then((json) => {
+        addPosts(json); // reactive value
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error, "fetching error");
+        setLoading(true);
+      });
+    return () => {
+      controller.abort(/*reason:?*/); // to kill async process in cleanup function
+    };
+  }, [addPosts]);
 
   return (
-    <PostContext value={{ deletepost, postState, addPosts , pushPost }}>
+    <PostContext value={{ deletepost, postState, loading, pushPost }}>
       {children}
     </PostContext>
   );
