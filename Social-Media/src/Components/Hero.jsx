10f1145046ee455529d81f597837df126b7951ea
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-04 15:41:00 +0500

    contain loader on async process and map on PostList

--- Social-Media/src/Components/Hero.jsx
+++ Social-Media/src/Components/Hero.jsx
@@ -1,42 +1,17 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import PostContext from "../Context/ContextApi";
 import Post from "./post";
-import Leader from "./Spinner";
+import Loader from "./Spinner";
 
 export default function Hero() {
-  const { postState, addPosts } = useContext(PostContext);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    fetch("https://jsonplaceholder.typicode.com/posts", { signal }).then((res) => {
-        if (!res.ok) {
-          throw new Error("error dua to unstable network");
-        } else {
-          return res.json();
-        }
-      }).then((json) => {
-        addPosts(json); // reactive value
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error,"fetching error");
-        setLoading(true);
-      });
-    return () => {
-      controller.abort(/*reason:?*/); // to kill async process in cleanup function
-    };
-  }, [addPosts]);
-
+  const { postState , loading } = useContext(PostContext);
+ 
   return (
     <>
       <div className="content">
-        
         {loading && (
           <div className="leading">
-            <Leader />
+            <Loader />
           </div>
         )}
 
