80d2b214a062760786c597ce852cb38eff70908a
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-12 20:59:35 +0500

    UserContext.provider  warp all the component inside and remove the footer page

--- Social-Media/src/App.jsx
+++ Social-Media/src/App.jsx
@@ -1,37 +1,19 @@
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import Hero from "./Components/Hero";
-import Signin from "./Components/Signin";
-import CreatePost from "./Components/New";
-import { useState } from "react";
 import { PostContextProvider } from "./Context/ContextProvider";
+import Container from "./Components/Container";
+import { Outlet } from "react-router-dom";
 function SocialMedia() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isCLicked, setIsClicked] = useState(false);
-
-  function HandleSign(){
-    setIsLoggedIn(true);
-  }
-  function HandleCLick() {
-    setIsClicked(true);
-  }
-  
-  function HandleNonClicked() {
-    setIsClicked(false);
-  }
+ 
 
   return (
     <>
       <PostContextProvider>
-        {!isLoggedIn ? (
-          <Signin onSignin={HandleSign} />
-        ) : (
-          <div className="fulcontainer">
-            <Header isCLicked={HandleCLick} NonClicked={HandleNonClicked} />
-            {isCLicked ? <CreatePost /> : <Hero />}
-            <Footer />
-          </div>
-        )}
+        <Container>
+          <Header />
+          <Outlet />
+          {/* <Footer /> */}
+        </Container>
       </PostContextProvider>
     </>
   );
