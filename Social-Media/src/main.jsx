f2b4b702764b2f0d70cf4e3cb65a1e66c758a0e2
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-12 20:57:33 +0500

    bootstrap plugin and configure of react-router

--- Social-Media/src/main.jsx
+++ Social-Media/src/main.jsx
@@ -1,18 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-//import { createBrowserRouter, RouterProvider } from "react-router";
-import './index.css'
-import App from './App.jsx'
+import { lazy, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Loader from "./Components/Spinner.jsx";
 
-// const router = createBrowserRouter([{
-//  path:"/" , element: <App />
-// },
-// ]);
+const App = lazy(() => import("./App.jsx"));
+const Hero = lazy(() => import("./Components/Hero.jsx"));
+const CreatePost = lazy(() => import("./Components/New.jsx"));
+const About = lazy(() => import("./Components/about.jsx"));
+const Feature = lazy(() => import("./Components/feature.jsx"));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <App />
-  </StrictMode>,
-)
+const root = window.document.getElementById("root");
+
+createRoot(root).render(
+  <BrowserRouter>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route element={<App />}>
+          <Route index element={<Hero />} />
+          <Route path="create-post" element={<CreatePost />} />
+          <Route path="about" element={<About />} />
+          <Route path="feature" element={<Feature />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  </BrowserRouter>
+);
