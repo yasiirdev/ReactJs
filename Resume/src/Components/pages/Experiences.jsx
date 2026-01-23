492889138d09ad9b8f1c788a422f46756f11f474
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-01-23 19:58:15 +0500

    experiences page with two components

---
+++ Resume/src/Components/pages/Experiences.jsx
@@ -0,0 +1,11 @@
+import WorkExperience from "../WorkExperience";
+import Worked from "../Worked";
+
+export default function ExperiencePage() {
+    return (
+      <div className="w-full">
+        <Worked />
+        <WorkExperience />
+      </div>
+    );
+}
