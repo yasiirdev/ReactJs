9eeb46a97a86c2cc5ba62ceac34d5bfc955a292a
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-03-06 19:31:27 +0500

    loader for myntra pages

---
+++ Myntra-clone/Myntra-frontend/src/components/loader.jsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-7 h-7 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loader;
