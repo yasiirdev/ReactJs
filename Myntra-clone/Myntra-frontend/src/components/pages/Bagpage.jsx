01043463b7812ff5fde157cdb733377efcbf91c2
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-03-15 13:48:04 +0500

    useSelector to select bagItems ,items and using condition to show bag items and empty items

--- Myntra-clone/Myntra-frontend/src/components/pages/Bagpage.jsx
+++ Myntra-clone/Myntra-frontend/src/components/pages/Bagpage.jsx
@@ -1,5 +1,38 @@
+import { useSelector } from "react-redux";
+import Product from "../product";
+
 export default function Bag_page() {
+  const bagItems = useSelector((state) => state.bagSlice);
+  const { items } = useSelector((state) => state.apiSlice);
+
+  if (items.length > 0) {
+    const finalItems = items[0].filter((item) => {
+      return bagItems.indexOf(item.id) >= 0;
+    });
+
     return (
-        <div className="w-full h-screen bg-white"></div>
-    )
+      <>
+        {bagItems.length > 0 ? (
+          <div className="w-full h-auto bg-white flex">
+            <div className="p-2 w-[80%] bg-white grid grid-cols-4 gap-8 ">
+              {finalItems.map((items, idx) => (
+                <Product key={idx} product={items} />
+              ))}
+            </div>
+            <div className="w-[20%] bg-blue-400 h-screen">
+              <div></div>
+            </div>
+          </div>
+        ) : (
+          <div className="w-full h-screen flex justify-center items-center">
+            <img
+              className=" size-lvh"
+              src="/emptyshopping.avif"
+              alt="myntra bag"
+            />
+          </div>
+        )}
+      </>
+    );
+  }
 }
