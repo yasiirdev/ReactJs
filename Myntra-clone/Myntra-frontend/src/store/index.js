0e163e79af7e1b97069ef27d4e46920f84d1cce3
yasiirdev <185803128+yasiirdev@users.noreply.github.com>
2026-03-15 13:38:01 +0500

    creating configuraStore and imporing apireducer , imagereducer and bagSlicereducder

--- Myntra-clone/Myntra-frontend/src/store/index.js
+++ Myntra-clone/Myntra-frontend/src/store/index.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import apiReducer from "./apiSlice";
 import imageReducer from "./mainImageSlice";
+import bagSliceReducer from "./bagSlice";
 const Store = configureStore({
   reducer: {
     apiSlice: apiReducer,
-    imageSlice : imageReducer,
+    imageSlice: imageReducer,
+    bagSlice: bagSliceReducer,
   },
 });
 
